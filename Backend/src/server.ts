f5b9241a2d8ea10702e6e6cf0f1cf3cbadc92671
wesley_wisch <wesley_wisch@hotmail.com>
2022-11-04 08:50:21 -0300

    Rota para  criação de um novo bolão.

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,9 +1,11 @@
 import Fastify from "fastify";
-import cors from '@fastify/cors';
-import { PrismaClient } from '@prisma/client';
+import cors from "@fastify/cors";
+import { z } from "zod";
+import { PrismaClient } from "@prisma/client";
+import ShortUniqueId from "short-unique-id";
 
 const prisma = new PrismaClient({
-  log: ['query'],
+  log: ["query"],
 });
 
 async function bootstrap() {
@@ -13,15 +15,38 @@ async function bootstrap() {
 
   await fastify.register(cors, {
     origin: true,
-  })
-
-  fastify.get('/pools/count', async () => {
-    const count = await prisma.pool.count();
-
-    return { count }
   });
 
-  await fastify.listen({ port: 3333, host: '0.0.0.0' });
+  fastify.get("/pools/count", async () => {
+    const count = await prisma.pool.count();
+
+    return { count };
+  });
+
+  fastify.post("/pools", async (req, res) => {
+    try {
+      const createPoolBody = z.object({
+        title: z.string(),
+      });
+
+      const { title } = createPoolBody.parse(req.body);
+      const generate = new ShortUniqueId({ length: 6 });
+      const code = String(generate()).toUpperCase();
+
+      await prisma.pool.create({
+        data: {
+          title,
+          code,
+        },
+      });
+
+      return res.status(201).send({ code });
+    } catch (err) {
+      return res.status(400).send({ error: "Sintaxe invalida" });
+    }
+  });
+
+  await fastify.listen({ port: 3333, host: "0.0.0.0" });
 }
 
 bootstrap();
