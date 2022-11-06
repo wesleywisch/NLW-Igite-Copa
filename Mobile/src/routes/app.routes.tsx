2c008c4f51a8788b7d19b74f8c389a4ef863f018
wesley_wisch <wesley_wisch@hotmail.com>
2022-11-06 16:31:35 -0300

    Tipagem de rotas

--- Mobile/src/routes/app.routes.tsx
+++ Mobile/src/routes/app.routes.tsx
@@ -5,6 +5,7 @@ import { PlusCircle, SoccerBall } from 'phosphor-react-native';
 
 import { New } from '../screens/New';
 import { Pools } from '../screens/Pools';
+import { Find } from '../screens/Find';
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
@@ -46,6 +47,12 @@ export function AppRoutes() {
           tabBarIcon: ({ color }) => <SoccerBall color={color} size={size} />,
           tabBarLabel: 'Meus bolões'
         }}
+      />
+
+      <Screen
+        name='find'
+        component={Find}
+        options={{ tabBarButton: () => null }}
       />
     </Navigator>
   );
