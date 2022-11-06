3dedbc024cfda543ee3905ee1810467081ac92f6
wesley_wisch <wesley_wisch@hotmail.com>
2022-11-06 10:32:57 -0300

    Criando a interface para criar um novo bolão, e também a interface para procurar um bolão já existente.

--- Mobile/App.tsx
+++ Mobile/App.tsx
@@ -3,7 +3,7 @@ import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@
 
 import { Loading } from './src/components/Loading';
 
-import { New } from './src/screens/New';
+import { Find } from './src/screens/Find';
 
 import { AuthContextProvider } from './src/contexts/AuthContext';
 
@@ -21,7 +21,7 @@ export default function App() {
           translucent
         />
 
-        {fontsLoaded ? <New /> : <Loading />}
+        {fontsLoaded ? <Find /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
